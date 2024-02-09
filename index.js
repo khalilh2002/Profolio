d2bfd6a0b9f98efc9cc65d496b9379c9bedf2d98
khalilh2002 <khalil.houssein2002@gmail.com>
2024-02-09 23:38:02 +0100

    added js for responsive top button

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ let topbutton = document.getElementById("top-button");
 topbutton.onclick=function () {
     document.documentElement.scrollTop=0;
 }
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop < 7) {
+        topbutton.style.display="none"
+    }else{
+        topbutton.style.display="block"        
+    }
+}  
