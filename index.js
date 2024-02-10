2633e297aa12a837849c4a2b23c956d4c92ec426
khalilh2002 <khalil.houssein2002@gmail.com>
2024-02-10 01:44:33 +0100

    navbar responded

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*--------------top button -----------------------------------*/ 
+/*-------------------------navbar------------------------*/
 
 let topbutton = document.getElementById("top-button");
 
@@ -5,10 +7,25 @@ topbutton.onclick=function () {
     document.documentElement.scrollTop=0;
 }
 
+let navbar = document.getElementById("header");
+
+let prevPosition = window.scrollY;
 window.onscroll = function () {
-    if (document.documentElement.scrollTop < 7) {
-        topbutton.style.display="none"
-    }else{
-        topbutton.style.display="block"        
+    let currentPosition= window.scrollY;
+
+    if (currentPosition < prevPosition) {
+        navbar.style.top = "0";
+    }else if (currentPosition > prevPosition){
+        navbar.style.top = "-100px";
     }
-}  
+    prevPosition=currentPosition;
+    
+    
+    /*-------------- top button------------*/
+    if (document.documentElement.scrollTop < 7) {
+        topbutton.style.display="none";
+    }else{
+        topbutton.style.display="block";    
+    }
+}
+
